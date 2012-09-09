c8a3eab9d9f25554826539b1e2cfa5973b3d9bf2
wickhamlam <wickhamlam@gmail.com>
2012-09-09 17:09:22 +0800

    test2

--- src/com/test/hello/HelloAndroidActivity.java
+++ src/com/test/hello/HelloAndroidActivity.java
@@ -8,6 +8,11 @@ public class HelloAndroidActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        
+        
+        
+        
         setContentView(R.layout.main);
     }
 }
